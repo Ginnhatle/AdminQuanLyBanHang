c0757b25d96499d489f21fe31d227bb1c5ae1c94
Ginnhatle <93597966+Ginnhatle@users.noreply.github.com>
2023-06-03 18:42:54 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Home from "./pages/Home";
-import ProductDetails from "./pages/ProductDetails";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import Footer from "./components/Footer";
 import {Login} from "./pages/Login";
 import AuthService from "./service/auth.service";
 
@@ -28,15 +23,12 @@ const App = () => {
                 <Routes>
                     <Route path={'/'} element={<Login/>}/>
                     {/*{currentUser?*/}
-                        <Route path={'/home'} element={<Home/>}/>
+                    {/*    <Route path={'/home'} element={<Home/>}/>*/}
                     {/*:("")*/}
                     }
-                    {/*{currentUser?*/}
-                        <Route path={'/product/:id'} element={<ProductDetails/>}/>
-                    {/*:("")*/}
+
                     }
                 </Routes>
-                <Sidebar/>
             </Router>
 
         </div>
